f50da66aaf32d32b751be18a42cde29aa575de80
Stefan Olivier <s.olivier1194@gmail.com>
2023-01-02 23:16:15 +0200

    added common.hpp header and source for examples added basic ArgumentParser & helpers for parsing user input

---
+++ examples/src/common.cpp
@@ -0,0 +1,23 @@
+// Copyright (C) Stefan Olivier
+// <https://stefanolivier.com>
+// ----------------------------
+// _description: Implementation for common.hpp for cxxtc examples
+
+// Standard headers
+#include <iostream>
+#include <vector>
+#include <string_view>
+#include <stdexcept>
+
+// Library headers
+#include "common.hpp"
+
+void print_usage()
+{
+    std::cout << "NOT IMPLEMENTED\n";
+}
+
+void print_help()
+{
+    std::cout << "NOT IMPLEMENTED\n";
+}
